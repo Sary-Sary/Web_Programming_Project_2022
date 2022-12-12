2b7b2518c49abf48e2556fc0ba3b90fe454f856a
Sary-Sary <56128679+Sary-Sary@users.noreply.github.com>
2022-12-12 22:27:38 +0200

    Add files via upload

--- Enemy.cpp
+++ Enemy.cpp
@@ -1,1 +1,90 @@
 #include "Enemy.h"
+
+Enemy::Enemy() {}
+Enemy::Enemy(int row_position, int col_position, Maze* maze) : Entity (row_position, col_position) {
+
+	maze->set_position(row_position, col_position, row_position, col_position, enemy);
+
+}
+
+void Enemy::move(Maze* maze, char direction) {
+
+	switch (direction) {
+	case 'w': move_up(maze); break;
+	case 'a': move_left(maze); break;
+	case 's': move_down(maze); break;
+	case 'd': move_right(maze); break;
+	}
+
+	return;
+
+}
+
+void Enemy::move_up(Maze* maze) {
+
+	if (position.get_row_position() + 1 >= maze->get_size()) return;
+
+	if (maze->get_position_by_coordinates(position.get_row_position() + 1, position.get_col_position()) == wall) return;
+
+	if (maze->get_position_by_coordinates(position.get_row_position() + 1, position.get_col_position()) == free_space) {
+
+		maze->set_position(position.get_row_position(), position.get_col_position(), position.get_row_position() + 1, position.get_col_position(), enemy);
+		position.move_down();
+
+	}
+
+	return;
+
+}
+
+void Enemy::move_down(Maze* maze) {
+
+	if (position.get_row_position() - 1 < 0) return;
+
+	if (maze->get_position_by_coordinates(position.get_row_position() - 1, position.get_col_position()) == wall) return;
+
+	if (maze->get_position_by_coordinates(position.get_row_position() - 1, position.get_col_position()) == free_space) {
+
+		maze->set_position(position.get_row_position(), position.get_col_position(), position.get_row_position() - 1, position.get_col_position(), enemy);
+		position.move_up();
+
+	}
+
+	return;
+
+}
+
+void Enemy::move_left(Maze* maze) {
+
+	if (position.get_col_position() - 1 < 0) return;
+
+	if (maze->get_position_by_coordinates(position.get_row_position(), position.get_col_position() - 1) == wall) return;
+	
+	if (maze->get_position_by_coordinates(position.get_row_position(), position.get_col_position() - 1) == free_space) {
+
+		maze->set_position(position.get_row_position(), position.get_col_position(), position.get_row_position(), position.get_col_position() - 1, enemy);
+		position.move_left();
+
+	}
+
+	return;
+
+}
+
+void Enemy::move_right(Maze* maze) {
+
+	if (position.get_col_position() + 1 >= maze->get_size()) return;
+
+	if (maze->get_position_by_coordinates(position.get_row_position(), position.get_col_position() + 1) == wall) return;
+
+	if (maze->get_position_by_coordinates(position.get_row_position(), position.get_col_position() + 1) == free_space) {
+
+		maze->set_position(position.get_row_position(), position.get_col_position(), position.get_row_position(), position.get_col_position() + 1, enemy);
+		position.move_right();
+
+	}
+
+	return;
+
+}
+
--- Enemy.h
+++ Enemy.h
@@ -6,7 +6,15 @@ class Enemy : private Entity
 	
 public:
 
-	Enemy ()
+	Enemy();
+	Enemy(int row_position, int col_position, Maze* maze);
+
+	virtual void move(Maze* maze, char direction);
+
+	virtual void move_up(Maze* maze);
+	virtual void move_down(Maze* maze);
+	virtual void move_left(Maze* maze);
+	virtual void move_right(Maze* maze);
 
 };
 
